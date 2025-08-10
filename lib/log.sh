feaa081726a34c5720a70e9c3f8e29701d85f432
Rann Day <5001592+rannday@users.noreply.github.com>
2025-08-09 22:06:04 -0400

    stdout issue

--- lib/log.sh
+++ lib/log.sh
@@ -3,7 +3,7 @@
 
 log_ts() { date +"[%Y-%m-%d %H:%M:%S]"; }
 
-info()  { printf '%s \e[32m[INFO]\e[0m %s\n'  "$(log_ts)" "$@"; }
-warn()  { printf '%s \e[33m[WARN]\e[0m %s\n'  "$(log_ts)" "$@"; }
-error() { printf '%s \e[31m[ERROR]\e[0m %s\n' "$(log_ts)" "$@"; }
+info()  { printf '%s \e[32m[INFO]\e[0m %s\n'  "$(log_ts)" "$@" >&2; }
+warn()  { printf '%s \e[33m[WARN]\e[0m %s\n'  "$(log_ts)" "$@" >&2; }
+error() { printf '%s \e[31m[ERROR]\e[0m %s\n' "$(log_ts)" "$@" >&2; }
 fatal() { printf '%s \e[31m[FATAL]\e[0m %s\n' "$(log_ts)" "$@" >&2; exit 1; }
